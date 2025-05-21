04af71b5872eeb4fddd7d1a8863d6c6a87d167b0
Benon78 <wilbenjamin7@gmail.com>
2025-05-21 16:13:52 +0300

    creating the navbar

--- fourteen_general_supply_e-commerce/src/App.jsx
+++ fourteen_general_supply_e-commerce/src/App.jsx
@@ -1,34 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import './App.css'
-
+import NavBar from './Components/NavBar/NavBar.jsx'
+import Shop from './Pages/Shop.jsx'
+import Product from './Pages/Product';
+import Contact from './Pages/Contact';
+import LoginSignup from './Pages/LoginSignup';
+import Cart from './Pages/Cart';
+import About from './Pages/About';
+import NotFoundPage from './Pages/NotFoundPage.jsx';
+import ProductDetail from './Components/ProductDetail/ProductDetail';
+import ProductCategory from './Pages/ProductCategory';
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  <BrowserRouter>
+    <NavBar/>
+    <Routes>
+      <Route path='/shop' element={<Shop/>}/>
+      <Route path='/products' element={<Product/>}>
+        <Route path='laptops' element={<ProductCategory category='laptops'/>}/>
+        <Route path='tablets' element={<ProductCategory category='tablets'/>}/>
+        <Route path='desktops' element={<ProductCategory category='desktops'/>}/>
+        <Route path='mobile-phones' element={<ProductCategory category='mobile-phones'/>}/>
+        <Route path='accessories' element={<ProductCategory category='accessories'/>}/>
+        <Route path='printers' element={<ProductCategory category='printers'/>}/>
+      </Route>
+      <Route path='/products/:productId' element={<ProductDetail/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      <Route path='/login' element={<LoginSignup/>}/>
+      <Route path='/cart' element={<Cart/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='*' element={<NotFoundPage/>}/>
+    </Routes>
+  </BrowserRouter>
   )
 }
 
---
+++ fourteen_general_supply_e-commerce/src/Pages/Product.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom"
+
+const Product = () => {
+  return (
+    <div>
+      Product
+      <Outlet/>
+    </div>
+  )
+}
+
+export default Product

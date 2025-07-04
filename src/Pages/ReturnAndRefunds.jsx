961e9a80f4ebb1a94ff4bc6f3bd0ac212b120a84
Benon78 <wilbenjamin7@gmail.com>
2025-07-04 12:45:39 +0300

    upade the dynamic page navigation

---
+++ src/Pages/ReturnAndRefunds.jsx
@@ -0,0 +1,11 @@
+
+import { usePageTittle } from '../Components/utils/Helper'
+
+function ReturnAndRefunds() {
+    usePageTittle('Return and Refunds - Fourteen General Supply','return-and-refunds')
+  return (
+    <div>ReturnAndRefunds</div>
+  )
+}
+
+export default ReturnAndRefunds
